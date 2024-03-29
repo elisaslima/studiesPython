ce4ce1f51a10cfcf4c43183605398b23a21c4cf9
elisaslima <134293077+elisaslima@users.noreply.github.com>
2024-03-29 20:11:42 -0300

    Adding the first lab of MC102 into my repository.

---
+++ lab01.py
@@ -0,0 +1,18 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 1 - Aritmética com Inteiros
+# Nome: Ana Elisa da Silva Lima
+# RA: 204409
+###################################################
+
+a = int(input())
+b = int(input())
+
+print("a =", a)
+print("b =", b)
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a ** b =", a ** b)
+print("a // b =", a // b)
+print("a % b =", a % b)
